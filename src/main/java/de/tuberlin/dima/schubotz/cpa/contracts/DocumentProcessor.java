95bae992b7e7a3b296e8c17ff1388e5eead6031f
physikerwelt (Moritz Schubotz) <wiki@physikerwelt.de>
2014-06-10 11:52:15 +0200

    Move logic from input format to mapper

--- src/main/java/de/tuberlin/dima/schubotz/cpa/contracts/DocumentProcessor.java
+++ src/main/java/de/tuberlin/dima/schubotz/cpa/contracts/DocumentProcessor.java
@@ -17,9 +17,14 @@
 package de.tuberlin.dima.schubotz.cpa.contracts;
 
 import de.tuberlin.dima.schubotz.cpa.types.WikiDocument;
+import de.tuberlin.dima.schubotz.cpa.utils.StringUtils;
 import eu.stratosphere.api.java.record.functions.MapFunction;
 import eu.stratosphere.types.Record;
+import eu.stratosphere.types.StringValue;
 import eu.stratosphere.util.Collector;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class DocumentProcessor extends MapFunction {
 
@@ -29,9 +34,25 @@ public class DocumentProcessor extends MapFunction {
     @Override
     public void map(Record record, Collector<Record> collector) {
 
-        WikiDocument doc = record.getField(0, WikiDocument.class);
+        String content = record.getField(0, StringValue.class).getValue();
+
+        // search for a page-xml entity
+        Pattern pageRegex = Pattern.compile("(?:<page>\\s+)(?:<title>)(.*?)(?:</title>)\\s+(?:<ns>)(.*?)(?:</ns>)\\s+(?:<id>)(.*?)(?:</id>)(?:.*?)(?:<text.*?>)(.*?)(?:</text>)", Pattern.DOTALL);
+        Matcher m = pageRegex.matcher(content);
+        // if the record does not contain parsable page-xml
+        if (!m.find()) return;
+
+        // otherwise create a WikiDocument object from the xml
+        WikiDocument doc = new WikiDocument();
+        doc.setId(Integer.parseInt(m.group(3)));
+        doc.setTitle(m.group(1));
+        doc.setNS(Integer.parseInt(m.group(2)));
+        doc.setText(StringUtils.unescapeEntities(m.group(4)));
+
+        // skip docs from namespaces other than
+        if (doc.getNS() != 0) return;
         doc.collectLinks(collector);
-        //LOG.info( "Analyzed Page '"+ doc.getTitle() );
+
     }
 }
 
--- src/main/java/de/tuberlin/dima/schubotz/cpa/io/WikiDocumentEmitter.java
+++ src/main/java/de/tuberlin/dima/schubotz/cpa/io/WikiDocumentEmitter.java
@@ -16,15 +16,8 @@
  */
 package de.tuberlin.dima.schubotz.cpa.io;
 
-import de.tuberlin.dima.schubotz.cpa.types.WikiDocument;
-import de.tuberlin.dima.schubotz.cpa.utils.StringUtils;
 import eu.stratosphere.api.java.record.io.TextInputFormat;
 import eu.stratosphere.configuration.Configuration;
-import eu.stratosphere.types.Record;
-import eu.stratosphere.types.StringValue;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * @author rob
@@ -36,35 +29,6 @@ public class WikiDocumentEmitter extends TextInputFormat {
         parameter.setString(DEFAULT_CHARSET_NAME, "UTF-8");
         super.configure(parameter);
         this.setDelimiter("</page>");
-    }
-
-    /* (non-Javadoc)
-     * @see eu.stratosphere.pact.common.io.DelimitedInputFormat#readRecord(eu.stratosphere.pact.common.type.Record, byte[], int)
-     */
-    @Override
-    public Record readRecord(Record target, byte[] bytes, int offset, int numBytes) {
-
-        super.readRecord(target, bytes, offset, numBytes);
-        String content = target.getField(0, StringValue.class).getValue();
-
-        // search for a page-xml entity
-        Pattern pageRegex = Pattern.compile("(?:<page>\\s+)(?:<title>)(.*?)(?:</title>)\\s+(?:<ns>)(.*?)(?:</ns>)\\s+(?:<id>)(.*?)(?:</id>)(?:.*?)(?:<text.*?>)(.*?)(?:</text>)", Pattern.DOTALL);
-        Matcher m = pageRegex.matcher(content);
-        // if the record does not contain parsable page-xml
-        if (!m.find()) return target;
-
-        // otherwise create a WikiDocument object from the xml
-        WikiDocument doc = new WikiDocument();
-        doc.setId(Integer.parseInt(m.group(3)));
-        doc.setTitle(m.group(1));
-        doc.setNS(Integer.parseInt(m.group(2)));
-        doc.setText(StringUtils.unescapeEntities(m.group(4)));
-
-        // skip docs from namespaces other than
-        if (doc.getNS() != 0) return target;
-
-        target.setField(0, doc);
-        return target;
     }
 
 }
