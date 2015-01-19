e0f78d169564f26a528eb283985d8b7db48cf37d
physikerwelt (Moritz Schubotz) <wiki@physikerwelt.de>
2015-01-19 18:07:20 +0100

    Tune config
    * tune config since job did not complete in about 40 minutes

--- support/malte-start.sh
+++ support/malte-start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 /share/hadoop/schubotz/flink-0.7.0-incubating/bin/start-cluster.sh && \
 /share/hadoop/schubotz/flink-0.7.0-incubating/bin/flink run \
--p 180 -c de.tuberlin.dima.schubotz.cpa.WikiSim \
+-p 400 -c de.tuberlin.dima.schubotz.cpa.WikiSim \
 /share/hadoop/schubotz/cpa-0.0.1.jar \
  hdfs://cloud-11.dima.tu-berlin.de:60010/datasets/enwiki-latest-pages-meta-current.xml \
  hdfs://cloud-11.dima.tu-berlin.de:60010/user/hadoop/physikerwelt/output/malte/1 \
