200aa250314beaf1a90ade5efef3c990500822ae
mschwarzer <ms@mieo.de>
2015-01-19 10:40:57 +0100

    updated to MinMaxPriorityQueue

--- src/main/java/de/tuberlin/dima/schubotz/cpa/evaluation/operators/ListBuilder.java
+++ src/main/java/de/tuberlin/dima/schubotz/cpa/evaluation/operators/ListBuilder.java
@@ -1,17 +1,15 @@
 package de.tuberlin.dima.schubotz.cpa.evaluation.operators;
 
-import de.tuberlin.dima.schubotz.cpa.evaluation.types.ComparableResult;
 import de.tuberlin.dima.schubotz.cpa.evaluation.types.ListResult;
+import de.tuberlin.dima.schubotz.cpa.evaluation.types.WikiSimComparableResult;
 import de.tuberlin.dima.schubotz.cpa.types.list.StringListValue;
 import org.apache.flink.api.common.functions.GroupReduceFunction;
 import org.apache.flink.api.java.tuple.Tuple;
-import org.apache.flink.shaded.com.google.common.collect.Ordering;
+import org.apache.flink.shaded.com.google.common.collect.MinMaxPriorityQueue;
 import org.apache.flink.types.StringValue;
 import org.apache.flink.util.Collector;
 
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 
 /**
  * Transform result set into EvaluationResult format:
@@ -34,19 +32,17 @@ public class ListBuilder<SORT extends Comparable, IN extends Tuple> implements G
 
         StringListValue resultList = new StringListValue();
 
-        // Convert to ComparableResult
-        List<ComparableResult<SORT>> unsortedList = new ArrayList<>();
+        // Convert to ComparableResult, add to SortedList with fixed length
+        MinMaxPriorityQueue<WikiSimComparableResult<SORT>> queue = MinMaxPriorityQueue.maximumSize(maxListLength).create();
+
 
         while (iterator.hasNext()) {
             record = iterator.next();
-            unsortedList.add(new ComparableResult<SORT>((String) record.getField(0), (String) record.getField(1), (SORT) record.getField(2)));
+            queue.add(new WikiSimComparableResult<SORT>((String) record.getField(0), (String) record.getField(1), (SORT) record.getField(2)));
         }
 
-        // Sort list, get topK results
-        List<ComparableResult<SORT>> sortedList = Ordering.natural().greatestOf(unsortedList, maxListLength);
-
         // Add to ResultList
-        for (ComparableResult<SORT> item : sortedList) {
+        for (WikiSimComparableResult<SORT> item : queue) {
             resultList.add(new StringValue((String) item.getField(1)));
         }
 
