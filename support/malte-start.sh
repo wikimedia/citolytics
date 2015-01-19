14dfbe53e97df893b6843eb418c1b9ee15e6508e
physikerwelt (Moritz Schubotz) <wiki@physikerwelt.de>
2015-01-19 19:11:51 +0100

    Testrun
    * remove the machines that are marked as dead from the job
    * test with the same threshold as used in the test cluster

--- support/malte-start.sh
+++ support/malte-start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 /share/hadoop/schubotz/flink-0.7.0-incubating/bin/start-cluster.sh && \
 /share/hadoop/schubotz/flink-0.7.0-incubating/bin/flink run \
--p 400 -c de.tuberlin.dima.schubotz.cpa.WikiSim \
+-p 368 -c de.tuberlin.dima.schubotz.cpa.WikiSim \
 /share/hadoop/schubotz/cpa-0.0.1.jar \
  hdfs://cloud-11.dima.tu-berlin.de:60010/datasets/enwiki-latest-pages-meta-current.xml \
  hdfs://cloud-11.dima.tu-berlin.de:60010/user/hadoop/physikerwelt/output/malte/1 \
- 0,0.5,1.0,1.1,1.2,1.3,1.4,1.5 0 0 && \
+ 1.25 0 5 && \
  /share/hadoop/schubotz/flink-0.7.0-incubating/bin/start-cluster.sh
