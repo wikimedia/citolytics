485dc68a87e088cf5fe634992aeec1768b7ded36
mschwarzer <ms@mieo.de>
2017-06-15 14:13:32 +0900

    Clean up

--- src/main/java/org/wikipedia/citolytics/cpa/utils/WikiSimStringUtils.java
+++ src/main/java/org/wikipedia/citolytics/cpa/utils/WikiSimStringUtils.java
@@ -1,19 +1,3 @@
-/*        __
- *        \ \
- *   _   _ \ \  ______
- *  | | | | > \(  __  )
- *  | |_| |/ ^ \| || |
- *  | ._,_/_/ \_\_||_|
- *  | |
- *  |_|
- * 
- * ----------------------------------------------------------------------------
- * "THE BEER-WARE LICENSE" (Revision 42):
- * <rob ∂ CLABS dot CC> wrote this file. As long as you retain this notice you
- * can do whatever you want with this stuff. If we meet some day, and you think
- * this stuff is worth it, you can buy me a beer in return.
- * ----------------------------------------------------------------------------
- */
 package org.wikipedia.citolytics.cpa.utils;
 
 import org.apache.commons.lang3.StringEscapeUtils;
@@ -21,14 +5,7 @@ import org.apache.commons.lang3.text.translate.CharSequenceTranslator;
 import org.apache.commons.lang3.text.translate.EntityArrays;
 import org.apache.commons.lang3.text.translate.LookupTranslator;
 
-import java.util.ArrayList;
-import java.util.List;
-
-/**
- * @author rob
- */
 public class WikiSimStringUtils {
-
 
     /**
      * Unescapes special entity char sequences like &lt; to its UTF-8 representation.
@@ -44,10 +21,6 @@ public class WikiSimStringUtils {
         return StringEscapeUtils.unescapeHtml4(iso.translate(basic.translate(text)));
     }
 
-    public static String addCsvEnclosures(String value) {
-        return "\"" + value.replace("\\", "\\\\") + "\"";
-    }
-
     public static long hash(String string) {
         long h = 1125899906842597L; // prime
         int len = string.length();
@@ -56,24 +29,6 @@ public class WikiSimStringUtils {
             h = 31 * h + string.charAt(i);
         }
         return h;
-    }
-
-    public static ArrayList<Double> getDoubleListFromString(String delimitedString, String delimiterPattern) {
-        ArrayList<Double> list = new ArrayList<>();
-        String[] dbs = delimitedString.split(delimiterPattern);
-        for (String db : dbs) {
-            list.add(Double.valueOf(db));
-        }
-        return list;
-    }
-
-    public static boolean startsNotWith(String text, List<String> patterns) {
-        for (String stopPattern : patterns) {
-            if (text.startsWith(stopPattern)) {
-                return false;
-            }
-        }
-        return true;
     }
 
 }
--- src/test/java/org/wikipedia/citolytics/tests/PerformanceTests.java
+++ src/test/java/org/wikipedia/citolytics/tests/PerformanceTests.java
@@ -3,9 +3,6 @@ package org.wikipedia.citolytics.tests;
 import com.google.common.collect.MinMaxPriorityQueue;
 import org.junit.Ignore;
 import org.junit.Test;
-import org.wikipedia.citolytics.cpa.utils.WikiSimStringUtils;
-
-import java.util.regex.Pattern;
 
 /**
  * Local tests to measure performance.
@@ -138,29 +135,6 @@ public class PerformanceTests {
 
         long timeB = System.nanoTime() - startB;
         System.out.printf("Adding Double to MinMaxQueue took an average of %.1f us%n", timeB / (runs * listlength) / 1000.0);
-
-    }
-
-    @Ignore
-    @Test
-    public void DoubleListPerformance() {
-        int runs = 999999;
-        String testStr = "1.0|2.0|100|0.05|1245.67";
-        String delimiter = Pattern.quote("|");
-
-//        long start = System.nanoTime();
-//        for (int i = 0; i < runs; i++) {
-//            DoubleListValue.valueOf(testStr, delimiter);
-//        }
-//        long time = System.nanoTime() - start;
-//        System.out.printf("Parse DoubleListValue took an average of %.1f us%n", time / runs / 1000.0);
-
-        long startB = System.nanoTime();
-        for (int i = 0; i < runs; i++) {
-            WikiSimStringUtils.getDoubleListFromString(testStr, delimiter);
-        }
-        long timeB = System.nanoTime() - startB;
-        System.out.printf("Parse ArrayList<Double> took an average of %.1f us%n", timeB / runs / 1000.0);
 
     }
 }
